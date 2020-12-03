0ad7932a3f73ce646b46ee56cb5759aea5be7dd6
bonedaddy <boned4ddy@protonmail.com>
2020-12-02 23:30:05 -0800

    comment baking.sh since there is a lot going on

--- scripts/baking.sh
+++ scripts/baking.sh
@@ -3,7 +3,9 @@
 # used to do bulk parsing of pcap files checking for crackable materials
 # works best if pcap files are named in the format SSID_MAC 
 
+# directory containing pcap files
 PCAP_DIR="$1"
+# root directory where we will store pcaps and their corresponding hccapx files
 OUTPUT_DIR_BASE="$2"
 
 if [[ "$OUTPUT_DIR_BASE" == "" ]]; then
@@ -14,23 +16,23 @@ if [[ ! -d "$OUTPUT_DIR_BASE" ]]; then
     mkdir "$OUTPUT_DIR_BASE"
 fi
 
-FILES=$(cd "$PCAP_DIR" && ls -l *.pcap  | awk '{print $NF}')
-
+# parse over all pcap files
 for file in $PCAP_DIR/*.pcap; do
     # filename expansion https://www.gnu.org/software/bash/manual/bash.html#Filename-Expansion
-    JUST_NAME=${file##*/}
-    JUST_NAME_NO_EXT=${JUST_NAME%.*}
-    STORE_DIR="$OUTPUT_DIR_BASE/$JUST_NAME_NO_EXT"
-
+    JUST_NAME=${file##*/} # get just the filename
+    JUST_NAME_NO_EXT=${JUST_NAME%.*} # strip the extension
+    STORE_DIR="$OUTPUT_DIR_BASE/$JUST_NAME_NO_EXT" # directory to store the pcap and the hccapx file
     mkdir "$STORE_DIR"
-    
     cp "$file" "$STORE_DIR"
     
+    # capture all crackable key materials not just the best
     hcxpcapngtool --all -o "$STORE_DIR/capture.hccapx" "$STORE_DIR/$JUST_NAME"
-    # if this fails we weren't able to capture any crackable materials 
-    # so wipe out the data, however note that this might not be entirely accurate
+    
+    # if the above fails we weren't able to capture any crackable materials 
+    # however cap2hccapx might work, so try that as a fallback
     if [[ ! -f "$STORE_DIR/capture.hccapx" ]]; then
-        # try cap2hccapx although this will likely also not work
+        # if cap2hccapx fails, then we probably did not capture any crackable materials
+        # so nuke the data since it's useless
         cap2hccapx "$STORE_DIR/$JUST_NAME" "$STORE_DIR/capture.hccapx"
         if [[ $(du -hs "$STORE_DIR/capture.hccapx" | awk '{print $1}') == 0 ]]; then
             echo "pcap conversion failed for $JUST_NAME_NO_EXT"
